318dd7a9bf72afc7237208cfb32b1e6cad6c8cad
unknown <nebaprincewill237@gmail.com>
2025-06-04 08:25:17 +0100

    Creation of Next.js Project with TypeScript and Tailwind CSS as well as creation of component, interface, and assets folders

---
+++ alx-project-0x02/alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header: React.FC = () => {
+    return(
+        <div className="flex justify-center items-center">
+            <h1 className="text-7xl font-bolder">Layout Header</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ alx-project-0x02/alx-project-0x02/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return(
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl font-boldler">WELCOME TO MY AIRBnB PROJECT</h1>
+    </div>
+  )
+}
+
+export default Home;
