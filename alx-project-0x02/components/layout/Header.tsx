0dcba92e17051c191160150844fd58456b2b29b9
unknown <nebaprincewill237@gmail.com>
2025-06-04 08:30:45 +0100

    Creation of Next.js Project with TypeScript and Tailwind CSS as well as creation of component, interface, and assets folders

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,8 @@
 const Header: React.FC = () => {
     return(
         <div className="flex justify-center items-center">
-            <h1 className="text-7xl font-bolder">Layout Header</h1>
+            <h1 className="text-xl font-bolder">Header</h1>
+            <p>Here we can find a simple header</p>
         </div>
     )
 }
