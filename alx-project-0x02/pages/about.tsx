c0c9513a001d515b24c3d0e4e58968759b3ee2ae
unknown <nebaprincewill237@gmail.com>
2025-06-08 07:04:29 +0100

    Display of Button component on UI through /about

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,15 @@
+import Button from "@/components/common/Button";
+
 const About: React.FC = () => {
     return(
         <div className="flex justify-center align-center">
             <h1 className="text-2x font-light">About Page</h1>
+            
+                <Button className="bg-white text-black text-center mx-auto rounded-sm w-sm" title="Small" />
+
+                <Button className="bg-white text-black text-center mx-auto rounded-md w-md" title="Medium" />
+
+                <Button className="bg-white text-black text-center mx-auto rounded-full w-lg" title="Large" />
         </div>
     )
 }
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -2,7 +2,6 @@ import Card from "@/components/common/Card";
 import PostModal from "@/components/common/PostModal";
 import { useState } from "react";
 import { CardProps } from "@/interfaces";
-import Button from "@/components/common/Button";
 const HomePage: React.FC = () => {
 
     const [modal, setModal] = useState(false)
@@ -29,11 +28,8 @@ const HomePage: React.FC = () => {
         </div>
             <div className="flex justify-center gap-2 items-center">
                 <Card title="Villa" content="lorem isjbcuyg hbqgyxcybhkx "/>
-                <Button className="bg-white text-black text-center mx-auto rounded-sm w-sm" title="Small" />
                 <Card title="Duplex" content="lorem ipsum jarif sddeofedcujbuj jbyusshb" />
-                <Button className="bg-white text-black text-center mx-auto rounded-md w-md" title="Medium" />
                 <Card title="Appartment" content="guyga7sgc vxyugx qsvuyf jgvutyfx" />
-                <Button className="bg-white text-black text-center mx-auto rounded-full w-lg" title="Large" />
                 <Card title="Villa" content="lorem isjbcuyg hbqgyxcybhkx "/>
                 <Card title="Duplex" content="lorem ipsum jarif sddeofedcujbuj jbyusshb" />
             </div>
